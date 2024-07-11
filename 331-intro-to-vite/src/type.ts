196c2c21a987fa3282eb1c063ae8d1eaf4c9b974
Ball-Thitiwut <Thitiwut_Thitnuea@cmu.ac.th>
2024-07-11 18:25:32 +0700

    Q8.1-8.4

---
+++ 331-intro-to-vite/src/type.ts
@@ -0,0 +1,11 @@
+export interface Event {
+    id: number
+    category: String
+    title: String
+    description: String
+    location: string
+    data: string
+    time: string
+    petsAllowed: boolean
+    organizer: string
+}
