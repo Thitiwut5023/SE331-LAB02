80685c06ce3532c2a2bc6a243f41bede1866b24d
THITIWUT THITNUEA <Thitiwut_Thitnuea@cmu.ac.th>
2024-07-29 16:13:10 +0700

    Q2.7

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -2,7 +2,7 @@
 import EventCard from '@/components/EventCard.vue'
 import EventInfo from '@/components/Organizer.vue'
 import { type Event } from '@/types'
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, computed, watchEffect } from 'vue';
 import EventService from '@/services/EventService'
 
 const events = ref<Event[] | null>(null)
@@ -15,13 +15,16 @@ const props = defineProps({
 })
 const page = computed(() => props.page)
 onMounted(() => {
-  EventService.getEvents(2, page.value)
+  watchEffect(() => {
+    events.value = null
+    EventService.getEvents(2, page.value)
     .then((response) => {
       events.value = response.data
     })
     .catch((error) => {
       console.error('There was an error!', error)
     })
+  })
 })
 
 </script>
