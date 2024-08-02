ee78b9ff9b27308c6d48e7b256c1daddde1cf189
THITIWUT THITNUEA <Thitiwut_Thitnuea@cmu.ac.th>
2024-08-03 00:41:33 +0700

    Q5.1

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -6,6 +6,7 @@ import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
 import StudentListView from '@/views/StudentListView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from '@/views//NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -49,6 +50,17 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource',
+      component: NotFoundView ,
+      props: true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
     },
     {
       path: '/student',
