8bac91574b69091c96125664fbeb252ba0ce19ef
THITIWUT THITNUEA <Thitiwut_Thitnuea@cmu.ac.th>
2024-08-03 00:57:32 +0700

    Q5.2

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -7,6 +7,7 @@ import EventEditView from '@/views/event/EditView.vue'
 import StudentListView from '@/views/StudentListView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import NotFoundView from '@/views//NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -61,6 +62,12 @@ const router = createRouter({
       path: '/:catchAll(.*)',
       name: 'not-found',
       component: NotFoundView
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView ,
+      props: true
     },
     {
       path: '/student',
