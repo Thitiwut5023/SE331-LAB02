e62ccf528c63e9ff45329a2d7a545daefd4da7d4
THITIWUT THITNUEA <Thitiwut_Thitnuea@cmu.ac.th>
2024-08-08 12:09:29 +0700

    Q1.3

--- 331-intro-to-vite/src/stores/message.ts
+++ 331-intro-to-vite/src/stores/message.ts
@@ -3,5 +3,13 @@ import  { defineStore } from 'pinia'
 export const useMessageStore = defineStore('message', {
     state: (): MessageState => ({
         message: ''
-    })
+    }),
+    actions: {
+        updateMessage(massage: string): void {
+            this.message = massage
+        },
+        resetMessage(): void {
+            this.message = ''
+        }
+    }
 })
