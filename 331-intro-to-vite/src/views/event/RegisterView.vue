beb6365c3501352f52b76c5d38a0e8a4294eb6f0
THITIWUT THITNUEA <Thitiwut_Thitnuea@cmu.ac.th>
2024-08-08 12:13:15 +0700

    Q1.4

--- 331-intro-to-vite/src/views/event/RegisterView.vue
+++ 331-intro-to-vite/src/views/event/RegisterView.vue
@@ -2,18 +2,22 @@
 import { toRefs , defineProps } from 'vue';
 import { type Event } from '@/types'
 import { useRouter } from 'vue-router';
+import { useMessageStore } from '@/stores/message';
 const props = defineProps<{
     event: Event
     id: String
 }>()
 const { event } = toRefs(props)
 const router = useRouter()
+const store = useMessageStore()
 const register = () => {
-    // If...
-    // Push...
-    router.push({ name: 'event-detail-view' })
+    store.updateMessage('You are succesfully registered for ' + props.event.title)
+    setTimeout(() => {
+        store.resetMessage()
+    }, 3000)
+    router.push({ name: 'event-detail-view', params: { id: props.event.id } })
 }
-router.push({ name: 'event-detail-view' })
+
 </script>
 <template>
     <p>Register event here</p>
