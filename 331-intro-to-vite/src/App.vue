0ef7082e11b173c8eb6b042cf7e800a47dde90fd
THITIWUT THITNUEA <Thitiwut_Thitnuea@cmu.ac.th>
2024-08-09 14:36:52 +0700

    added speed-insights

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -3,7 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 import EventCard from './components/EventCard.vue'
 import { useMessageStore } from './stores/message';
 import { storeToRefs } from 'pinia';
-import { inject } from "@vercel/analytics"
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+injectSpeedInsights();
 const store = useMessageStore()
 const { message } = storeToRefs(store)
 </script>
